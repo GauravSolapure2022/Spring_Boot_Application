60464249093a3e697a878609a5997751d4121eab
Solapure <gaurav.solapure@harman.com>
2024-08-03 21:07:35 +0530

    commited by me

---
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -0,0 +1,33 @@
+package in.ashokit.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+
+	private Logger logger = LoggerFactory.getLogger(WelcomeRestController.class);
+
+	public WelcomeRestController() {
+		logger.info("***** WelcomeRestController :: Constructor *****");
+	}
+
+	@GetMapping
+	public String welcomeMsg() {
+		logger.info("***** welcomeMsg() execution start *****");
+		String msg = "Welcome to Ashok IT - Software Training Institute (HYD) ..!!";
+		logger.info("***** welcomeMsg() execution end *****");
+		return msg;
+	}
+	
+	@GetMapping("/greet")
+	public String greetMsg() {
+		logger.info("***** greetMsg() execution start *****");
+		String msg = "Good Morning, Welcome to Ashok IT";
+		logger.info("***** greetMsg() execution end *****");
+		return msg;
+	}
+}
