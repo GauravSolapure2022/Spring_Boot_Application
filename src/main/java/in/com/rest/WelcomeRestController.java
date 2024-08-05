48a02fe04f097b0bf139f3fe524ca07cdc6f7de3
Solapure <gaurav.solapure@harman.com>
2024-08-05 16:08:34 +0530

    Chages in restController

--- src/main/java/in/com/rest/WelcomeRestController.java
+++ src/main/java/in/com/rest/WelcomeRestController.java
@@ -18,7 +18,7 @@ public class WelcomeRestController {
 	@GetMapping
 	public String welcomeMsg() {
 		logger.info("***** welcomeMsg() execution start *****");
-		String msg = "Welcome to Codetech Carporate training and placement center Pune ..!!";
+		String msg = "Welcome to Codetech Carporate training and placement center Pune (Deccan Near Bus Stop) ..!!";
 		logger.info("***** welcomeMsg() execution end *****");
 		return msg;
 	}
